0d519f5fe079088a949d6653282412560518b972
hakander <hakanOC78@hotmail.com>
2019-01-22 06:36:42 +0100

    constantes.py

---
+++ constantes.py
@@ -0,0 +1,14 @@
+number_sprite_corner = 15
+size_sprite = 41
+window_corner = number_sprite_corner * size_sprite
+
+image_home = 'ressource/floor-tiles-20x20.png'
+image_empty = 'ressource/empty.png'
+image_wall = 'ressource/wall.png'
+image_start = 'ressource/start.png'
+image_arrive = 'ressource/arrive.png'
+macgyver = 'ressource/MacGyver.png'
+image_guardian = 'ressource/gardien.png'
+image_treasure = 'ressource/treasure.png'
+image_piece = 'ressource/piece.png'
+image_potion = 'ressource/potion.png'
